4b9004cb99a209a6cd4ac03532a27ca3c592fe9e
rekhinnvs <rekhin.nvs@gmail.com>
2017-02-07 14:57:45 +0530

    Modified Dictionary to for easy search

--- HackerRank/codes/Dictionary.py
+++ HackerRank/codes/Dictionary.py
@@ -5,18 +5,13 @@ for i in range(0,n):
     users = raw_input()
     name =users.split()
     tel[str(name[0])] = str(name[1])
-#print tel
 lists = []
 for i in range(0, n):
     lists.append(raw_input())
-#print lists
 for temp in lists:
-    for k, v in tel.items():
-        #print 'temp = ', temp
-        #print 'compare',temp,k
-        if temp == k:
-            print k, v
-            break
+    if temp in tel.keys():
+        print temp+'='+tel[temp]
+    else:
         print 'Not found'
 
 
