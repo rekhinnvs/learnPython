f4e7c73923029cb8e70849b20d7e60a44785ed27
rekhinnvs <rekhin.nvs@gmail.com>
2017-02-21 17:23:39 +0530

    Updated halloween script, Hackerrank

--- HackerRank/codes/Halloween.py
+++ HackerRank/codes/Halloween.py
@@ -1,8 +1,10 @@
 n = int(raw_input())
 data = []
 for i in range(0,n):
-	data[i] = int(raw_input())
+    data.append(int(raw_input()))
 for i in data:
-	row = i%2
-	column = i/2
-	print row*column
+    row = i/2
+    print 'row', row
+    column = i-row
+    print 'column', column
+    print row*column
